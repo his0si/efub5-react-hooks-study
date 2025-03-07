a36d9f3ceed69824141b2b7e480a903d1b8beddd
Hee Seo Kim <hisounni@ewhain.net>
2025-03-07 16:31:13 +0900

    1.2까지

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import useCounter from './hooks/useState';
+import useInput from './hooks/useInput';
+
+function App() {
+  const { item, increment, decrement } = useCounter(1);
+  const name = useInput('Mr.');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Item: {item}</h2>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+
+        <input placeholder="Name" {...name} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
