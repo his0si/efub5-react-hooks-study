2b231f89ccc388f0196e4066a3c6099e369a75a4
Hee Seo Kim <hisounni@ewhain.net>
2025-03-07 17:19:00 +0900

    1.3

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import useCounter from './hooks/useState';
 import useInput from './hooks/useInput';
+import useTabs from './hooks/useTabs';
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
 
 function App() {
   const { item, increment, decrement } = useCounter(1);
   const name = useInput('Mr.');
+  const { currentItem, changeItem } = useTabs(0, content);
 
   return (
     <div className="App">
@@ -14,6 +27,16 @@ function App() {
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
         <input placeholder="Name" {...name} />
+        <div>
+          {content.map((section, index) => (
+            <button key={index} onClick={() => changeItem(index)}>
+              {section.tab}
+            </button>
+          ))}
+        </div>
+        <div>
+          {currentItem ? currentItem.content : "No Content"}
+        </div>
       </header>
     </div>
   );
