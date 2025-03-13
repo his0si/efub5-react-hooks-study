5e4068a6c2a98efa8be12fb33254448293f7c4ba
Hee Seo Kim <hisounni@ewhain.net>
2025-03-14 01:31:23 +0900

    week1

--- client/src/hooks/useTabs.js
+++ client/src/hooks/useTabs.js
@@ -4,6 +4,7 @@ const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   // React Hooks는 모든 렌더링에서 항상 같은 순서로 호출 되어야 함
   // 항상 useState를 실행하도록 보장 
+  
   // Hook을 조건문 안에서 호출하는 대신, allTabs가 유효하지 않을 경우 기본값을 반환하도록 수정
   if (!Array.isArray(allTabs) || allTabs.length === 0) {
     return {
